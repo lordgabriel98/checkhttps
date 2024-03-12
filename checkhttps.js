f86451eadce494742eb1a18adc3d6a7b38298966
lordgabriel98 <bajegabriel@gmail.com>
2024-03-12 18:39:08 +1000

    Updated checkhttps.js and .gitignore

---
+++ checkhttps.js
@@ -0,0 +1,16 @@
+const fccUrl = new URL("https://www.pngec.gov.pg");
+
+console.log(fccUrl);
+
+/*
+const isValidUrl = (url)=>{
+	let fccUrl = new URL(url);
+	
+	if(fccUrl[protocol]==='https:'){
+		return true;	
+	} else{
+		return false;
+	} 
+}
+*/
+//isValidUrl('www.pngec.gov.pg');
