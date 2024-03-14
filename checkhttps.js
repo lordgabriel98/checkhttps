c65b0484a7cd53d433baeadf3fb2ae84de76364a
lordgabriel98 <bajegabriel@gmail.com>
2024-03-14 15:37:05 +1000

    alternative function

--- checkhttps.js
+++ checkhttps.js
@@ -1,50 +1,28 @@
-const tls = require('tls');
-const websitesData = require('./govWeb.json');
+ const https = require('https');
 
+//Function to check if the website has a valid SSL certificate
+const checkSSLValidity = (url) => {
+  https.get(url, (res) => {
+       const certificate = res.socket.getPeerCertificate();
 
-const checkSSL = async(hostname) =>{
-	return new Promise((resolve, reject)=>{
-		const options = {
+           // Check if the certificate object is not empty and valid
+               if (certificate && Object.keys(certificate).length) {
+                     const validTo = new Date(certificate.valid_to);
+                           const isValid = validTo > new Date(); // Check if the certificate is currently valid
 
-			host: hostname,
-			port: 443,
-			rejectUnauthorized: false,
-		};
+                                 if (isValid) {
+                                         console.log(`${url} has a valid SSL certificate.`);
+                                               } else {
+                                                       console.log(`${url}'s SSL certificate is expired.`);
+                                                             }
+                                                                 } else {
+                                                                       console.log(`${url} does not use SSL/TLS or the certificate could not be verified.`);
+                                                                           }
+                                                                             }).on('error', (e) => {
+                                                                                 console.error(`Error checking SSL certificate for ${url}: `, e);
+                                                                                   });
+                                                                                   };
 
-		const socket = tls.connect(options, () =>{
-			
-			const isSSL = socket instanceof tls.TLSSocket;
+          // Replace 'https://example.com' with the URL of the website you want to check
+checkSSLValidity('www.pngec.gov.pg');
 
-			socket.end();
-
-			resolve(isSSL);
-		});
-
-		socket.on('error', (error)=>{
-			reject(error);
-		});
-
-	});
-
-
-};
-
-
-const checkWebsites = async ()=>{ 
- const websites = Object.values(websitesData);
-
-for (let website of websites){
-	
-	try{
-		const isSSL = await checkSSL(website);
-		console.log(`${website} is using SSL: ${isSSL}`);
-	
-	}catch(error){
-		console.error(`Error checking SSL for ${website}`);
-			}
-									
-	}	
-}
-
-
-checkWebsites();
