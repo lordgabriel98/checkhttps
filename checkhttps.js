7950089cd605add4bbb624115295586fa3106512
lordgabriel98 <bajegabriel@gmail.com>
2024-03-14 18:17:26 +1000

    Fixed indentations on code

--- checkhttps.js
+++ checkhttps.js
@@ -7,22 +7,24 @@ const checkSSLValidity = (url) => {
 
            // Check if the certificate object is not empty and valid
                if (certificate && Object.keys(certificate).length) {
-                     const validTo = new Date(certificate.valid_to);
-                           const isValid = validTo > new Date(); // Check if the certificate is currently valid
+                   const validTo = new Date(certificate.valid_to);
+                   const isValid = validTo > new Date(); // Check if the certificate is currently valid
 
-                                 if (isValid) {
-                                         console.log(`${url} has a valid SSL certificate.`);
-                                               } else {
-                                                       console.log(`${url}'s SSL certificate is expired.`);
-                                                             }
-                                                                 } else {
-                                                                       console.log(`${url} does not use SSL/TLS or the certificate could not be verified.`);
-                                                                           }
-                                                                             }).on('error', (e) => {
-                                                                                 console.error(`Error checking SSL certificate for ${url}: `, e);
-                                                                                   });
-                                                                                   };
+                   if (isValid) {
+                         console.log(`${url} has a valid SSL certificate.`);
+                 } else {
+                         console.log(`${url}'s SSL certificate is expired.`);
+                 }
+            
+	       } else {
+               	         console.log(`${url} does not use SSL/TLS or the certificate could not be verified.`);
+                      }
 
-          // Replace 'https://example.com' with the URL of the website you want to check
+  			}).on('error', (e) => {
+                        
+			console.error(`Error checking SSL certificate for ${url}: `, e);
+                        });
+                    };
+
 checkSSLValidity('www.pngec.gov.pg');
 
