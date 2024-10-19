55716cdbb630550fbfe124b41891f3805ddc562a
lordgabriel98 <bajegabriel@gmail.com>
2024-10-19 14:47:51 +1000

    successful in returning verification

--- checkSSL.sh
+++ checkSSL.sh
@@ -5,5 +5,13 @@ array_size=$(($json_size-1))
 
 for i in $(seq 0 $array_size)
 do
-  jq --arg i "$i" 'keys[$i|tonumber]' govWeb.json
+  website=$(jq --arg i "$i" 'keys[$i|tonumber]' govWeb.json)
+  website=$(echo "$website" | xargs)
+  url=$(jq --arg website "$website" '.[$website]' govWeb.json)
+  url="$url" 
+  echo "Department: $website"
+  port=443
+  url=$(echo "$url" | awk '{gsub(/"/, ""); print}')
+  timeout 5 openssl s_client -connect $url:443 | grep Verification
+  echo $url
 done
