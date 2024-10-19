810852796046d91c27f2f167e57ff53781c515a7
lordgabriel98 <bajegabriel@gmail.com>
2024-10-19 13:42:12 +1000

    can successfully print all names of gov departments through a loop
    also edited the requirements to make the script executable

--- checkSSL.sh
+++ checkSSL.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-websites=`cat govWeb.json | jq`
+json_size=`jq length govWeb.json`
+array_size=$(($json_size-1))
 
-echo $websites
+for i in $(seq 0 $array_size)
+do
+  jq --arg i "$i" 'keys[$i|tonumber]' govWeb.json
+done
--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+# update apt or respective package manager and download jq to traverse json using bash
 sudo apt update && sudo apt install jq
+#make the checkSSL.sh script executable
+chmod +x checkSSL.sh
